c6df6e14b514caa07f2448b30fba1fc78a6206e1
Skye <Skye.vip@foxmail.com>
2019-03-24 16:04:25 +0800

    init project and some file

---
+++ src/test/java/com/gdut/graduation/dao/UserMapperTest.java
@@ -0,0 +1,35 @@
+package com.gdut.graduation.dao;
+
+import com.gdut.graduation.pojo.User;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+/**
+ * @Description
+ * @Author Skye
+ * @Date 2019/3/24 10:18
+ * @Version 1.0
+ **/
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class UserMapperTest {
+    @Autowired
+    private UserMapper userMapper;
+
+    @Test
+    public void getUserByIdTest(){
+
+//        User user = userMapper.getUserById(1);
+//        Assert.assertNotNull(user);
+    }
+    @Test
+    public void test(){
+        System.out.println(1);
+    }
+}
